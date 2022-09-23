259ad0051b26cd09f474ce640fa5b9ac7fdceb41
vivek mishra <starvivek1996@gmail.com>
2022-09-23 14:15:01 +0530

    updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "This is the first machine learning projects."
+    return "CI/CD pipe line has been establishid."
 
 
 if __name__=="__main__":
